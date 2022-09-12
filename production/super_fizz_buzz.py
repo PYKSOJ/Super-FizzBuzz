02fbaa279d87a63f15e0adcabdb5d86ac0a7ab3d
PYKSOJ <6210110216@psu.ac.th>
2022-09-13 01:03:52 +0700

    init super fizzbuzz

---
+++ production/super_fizz_buzz.py
@@ -0,0 +1,19 @@
+def super_fizz_buzz(number):
+    symbol = []
+    if number % 9 == 0 and number % 25 == 0 :
+        symbol.append('FizzFizzBuzzBuzz')
+    elif number % 3 == 0 and number % 5 == 0 :
+        symbol.append('FizzBuzz') 
+    elif number % 9 == 0 :
+        symbol.append('FizzFizz')
+    elif number % 3 == 0 :
+        symbol.append('Fizz')
+    elif number % 25 == 0 :
+        symbol.append('BuzzBuzz')
+    elif number % 5 == 0 :
+        symbol.append('Buzz')
+    else:
+        symbol.append('NoFizzBuzz')
+    tmp = ''.join(map(str, symbol))
+    return(tmp)
+    
